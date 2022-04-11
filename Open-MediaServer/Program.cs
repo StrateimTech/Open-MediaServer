2567a619037b8db0b1e2cdc0a108a1a6db4305c8
Etho <58352929+EthoIRL@users.noreply.github.com>
2022-04-11 15:16:41 -0700

    Updated target framework & Lang version

--- Open-MediaServer/Program.cs
+++ Open-MediaServer/Program.cs
@@ -7,23 +7,11 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace Open_MediaServer
+namespace Open_MediaServer;
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        private static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>().UseKestrel(options =>
-                    {
-                        options.Limits.MaxRequestBodySize = null;
-                    });
-                });
+        
     }
 }
