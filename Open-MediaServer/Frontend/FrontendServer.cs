6e6cc05041cb7ba39f843f31335396b1a77e8ea7
Etho <58352929+EthoIRL@users.noreply.github.com>
2022-05-30 11:51:44 -0700

    Fixed hot reload

--- Open-MediaServer/Frontend/FrontendServer.cs
+++ Open-MediaServer/Frontend/FrontendServer.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 
 namespace Open_MediaServer.Frontend;
