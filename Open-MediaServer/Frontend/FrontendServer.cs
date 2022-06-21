ce2477b5e35442fb5123394e348da11ed522c6c6
Etho <58352929+EthoIRL@users.noreply.github.com>
2022-06-21 15:42:40 -0700

    Upload limit for frontend

--- Open-MediaServer/Frontend/FrontendServer.cs
+++ Open-MediaServer/Frontend/FrontendServer.cs
@@ -47,6 +47,14 @@ public class FrontendServer
         {
             options.ListenAnyIP(Program.ConfigManager.Config.FrontendPorts.http);
             options.ListenAnyIP(Program.ConfigManager.Config.FrontendPorts.https, configure => configure.UseHttps());
+                
+            int? fileUploadMax = Program.ConfigManager.Config.FileNetworkUploadMax;
+            if (fileUploadMax != null)
+            {
+                fileUploadMax *= 100000000;
+            }
+
+            options.Limits.MaxRequestBodySize = fileUploadMax;
         });
 
         builder.Services.AddRazorPages(options =>
