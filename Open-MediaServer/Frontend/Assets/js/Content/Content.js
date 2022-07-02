543610b0a2601208b51b0a7ad51f46e7b8f82cbc
Etho <58352929+EthoIRL@users.noreply.github.com>
2022-07-01 23:21:05 -0700

    Added drag and drop function to content page

--- Open-MediaServer/Frontend/Assets/js/Content/Content.js
+++ Open-MediaServer/Frontend/Assets/js/Content/Content.js
@@ -21,3 +21,57 @@ function updateQueryURL(query, window) {
     
     window.location.href = url;
 }
+
+function dragOverHandler(ev) {
+    ev.preventDefault();
+}
+
+function dropHandler(ev, action) {
+    console.log(action);
+    ev.preventDefault();
+    if (ev.dataTransfer.files) {
+        let form = document.createElement("form");
+        form.method = "POST";
+        form.action = action;
+        form.enctype = "multipart/form-data";
+        for (let i = 0; i < ev.dataTransfer.files.length; i++) {
+            let file = ev.dataTransfer.files[i];
+            
+            let fileInput = document.createElement("input");
+            let fileName = document.createElement("input");
+            let filePrivate = document.createElement("input");
+            
+            fileInput.type = "file";
+            fileName.type = "text";
+            filePrivate.type = "checkbox";
+            
+            fileInput.name = "File " + i;
+            fileName.name = "Name " + i;
+            filePrivate.name = "Private " + i;
+
+            const dataTransfer = new DataTransfer();
+            dataTransfer.items.add(file);
+            fileInput.files = dataTransfer.files;
+            fileName.value = file.name;
+            filePrivate.checked = false;
+
+            fileInput.style.display = "none";
+            fileName.style.display = "none";
+            filePrivate.style.display = "none";
+
+            form.appendChild(fileInput);
+            form.appendChild(fileName);
+            form.appendChild(filePrivate);
+        }
+        
+        let returnURL = document.createElement("input");
+        returnURL.type = "hidden";
+        returnURL.name = "returnURL";
+        returnURL.value = "/Content";
+        returnURL.style.display = "none";
+        form.appendChild(returnURL);
+        
+        document.body.appendChild(form);
+        form.submit();
+    }
+}
---
+++ Open-MediaServer/Frontend/Assets/js/Upload/Upload.js
@@ -0,0 +1,49 @@
+﻿var id = 0;
+
+function dragOverHandler(ev) {
+    ev.preventDefault();
+}
+function dropHandler(ev) {
+    ev.preventDefault();
+    if (ev.dataTransfer.files) {
+        for (let i = 0; i < ev.dataTransfer.files.length; i++) {
+            let file = ev.dataTransfer.files[i];
+            id++;
+            addContainer(ev);
+            document.getElementById("Name " + id).value = file.name;
+            document.getElementById("File " + id).files = ev.dataTransfer.files;
+            itemChange(ev.dataTransfer, i, id);
+        }
+    }
+}
+
+function addContainer(ev) {
+    if (id >= 9) {
+        return;
+    }
+    document.getElementById("file-drag-container").insertAdjacentHTML("beforebegin", '<div class="file-upload">\n<div>\n<video class="file-upload-preview" id="Video Preview ' + id + '" muted></video>\n<img class="file-upload-preview" id="Img Preview ' + id + '"/>\n<div class="file-no-preview" id="No Preview ' + id + '">No file preview</div>\n</div>\n<div class="file-input-area">\n<div>\n<input class="file-upload-input" type="file" id="File ' + id + '" name="File '+ id +'" onchange="itemChange(this, 0, '+ id +');">\n<label class="file-upload-input-label" for="File ' + id +'">\n<span>Change file</span>\n</label>\n</div>\n<div>\n<div class="file-name-input-label">\n<span>Name</span>\n</div>\n<input class="file-name-input" type="text" id="Name '+ id + '" name="Name '+ id +'">\n</div>\n<div>\n<div class="file-private-input-label">\n<span>Private</span>\n</div>\n<input class="file-private-input" type="checkbox" id="Private ' + id + '" name="Private '+ id +'">\n</div>\n</div>\n</div>');
+}
+
+function itemChange(input, fileId, id) {
+    if (input.files) {
+        let file = input.files[fileId];
+        document.getElementById("Name " + id).value = file.name;
+        let fileReader = new FileReader();
+        fileReader.readAsDataURL(file);
+        fileReader.onload = function(event) {
+            let videoPreview =  document.getElementById("Video Preview " + id);
+            let imgPreview =  document.getElementById("Img Preview " + id);
+            let noPreview =  document.getElementById("No Preview " + id);
+            noPreview.style.display = "none";
+            if (file.type === "video/mp4" || file.type === "video/x-msvideo" || file.type === "video/quicktime" || file.type === "video/webm" || file.type === "video/x-ms-wmv") {
+                videoPreview.src = event.target.result;
+                videoPreview.style.display = "inline-block";
+                imgPreview.style.display = "none";
+                return;
+            }
+            imgPreview.src = event.target.result;
+            imgPreview.style.display = "inline-block";
+            videoPreview.style.display = "none";
+        };
+    }
+}
